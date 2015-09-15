5f25825935db88d11613f0b61b0a7fe3426f4807
Kyle Dampier <424camo@gmail.com>
2015-09-15 18:04:46 -0400

    Initial Commit

---
+++ Lucid Dreams/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lucid Dreams
+//
+//  Created by Kyle Dampier on 9/15/15.
+//  Copyright (c) 2015 Kyle Dampier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
